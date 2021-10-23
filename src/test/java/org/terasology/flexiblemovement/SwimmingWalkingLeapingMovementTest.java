b3f912861e9a295d00684bf3c251eb7195a185af
Arthur <arthur.casals@gmail.com>
2021-10-23 18:17:44 +0200

    Fixed imports on tests

--- src/test/java/org/terasology/flexiblemovement/SwimmingWalkingLeapingMovementTest.java
+++ src/test/java/org/terasology/flexiblemovement/SwimmingWalkingLeapingMovementTest.java
@@ -3,6 +3,30 @@
 package org.terasology.flexiblemovement;
 
 import org.junit.jupiter.api.Test;
+import com.google.common.collect.Sets;
+import org.joml.Vector3f;
+import org.joml.Vector3i;
+import org.joml.Vector3ic;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.engine.entitySystem.entity.EntityManager;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.logic.characters.CharacterMovementComponent;
+import org.terasology.engine.logic.characters.CharacterTeleportEvent;
+import org.terasology.engine.logic.location.LocationComponent;
+import org.terasology.engine.physics.engine.PhysicsEngine;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.world.WorldProvider;
+import org.terasology.engine.world.block.Block;
+import org.terasology.engine.world.block.BlockManager;
+import org.terasology.engine.world.block.BlockRegion;
+import org.terasology.engine.world.block.BlockRegionc;
+import org.terasology.engine.world.block.Blocks;
+import org.terasology.moduletestingenvironment.MTEExtension;
+import org.terasology.moduletestingenvironment.ModuleTestingHelper;
+import org.terasology.moduletestingenvironment.extension.Dependencies;
 
 @ExtendWith(MTEExtension.class)
 @Dependencies({"FlexibleMovement", "CoreAssets"})
